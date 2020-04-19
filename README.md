72f304d713c1dc455f5bd7254642bd2fdd73b4c3
ValeriaKravtsova <lera.gor@inbox.ru>
2020-04-19 20:32:41 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+issue-01
+вводим в терминал
+python -m doctest -v -o ELLIPSIS trying_dict.py
+
+issue-02
+вводим в терминал
+pytest morse.py::test_morse_to_english
+
+issue-03
+вводим в терминал
+python3 test_fit_transform_issue_3.py
---
+++ test_fit_transform_issue_3.py
@@ -0,0 +1,25 @@
+import unittest
+from one_hot_encode import fit_transform
+
+
+class TestFitTransform(unittest.TestCase):
+
+    def test_equal(self):
+        self.assertEqual(fit_transform('Moscow'), [('Moscow', [1])])
+
+    def test_not_equal(self):
+        self.assertNotEqual((fit_transform('Moscow')), (fit_transform('New York')))
+
+    def test_type(self):
+        self.assertIsInstance(fit_transform('Moscow'), list)
+
+    def test_if_none(self):
+        self.assertIsNotNone(fit_transform('New York'))
+
+    def test_raise_type_error(self):
+        self.assertRaises(TypeError, fit_transform([2, 3, 4]))
+        self.assertRaises(TypeError, fit_transform({'Moscow': 'Russia'}))
+
+
+if __name__ == '__main__':
+    unittest.main()
