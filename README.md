f8c67dd7c8dc6999a311ba52cbdc2ee829de73e8
ValeriaKravtsova <lera.gor@inbox.ru>
2020-04-21 16:55:24 +0300

    change some tests

--- README.md
+++ README.md
@@ -1,25 +1,26 @@
 issue-01
-вводим в терминал
+
 ```python3 -m doctest -v -o ELLIPSIS trying_dict.py```
 
 issue-02
-вводим в терминал
+
 ```pytest test_issues_1_2_morse.py::test_morse_to_english```
 
 issue-03
-вводим в терминал
+
 ```python3 test_issue_3_test_fit_transform.py```
 
 issue-04
-вводим в терминал
+
 ```pytest test_issue_4_one_hot_encode.py```
 
 issue-05
-вводим в терминал
+
 ```coverage run test_what_is_year_now.py
    coverage report -m
    coverage html```
 
-или 
-```python3 -m pytest --cov . --cov-report html
+
+   python3 -m pytest --cov
+   python3 -m pytest --cov . --cov-report html
 ```
--- test_issue_5_test_what_is_year.py
+++ test_issue_5_test_what_is_year.py
@@ -1,9 +1,6 @@
 import json
 import unittest
-import urllib.request
 
-
-import test_what_is_year_now
 from test_what_is_year_now import what_is_year_now
 from unittest.mock import MagicMock
 
@@ -18,9 +15,9 @@ class TestWhatIsYear(unittest.TestCase):
         json.load = MagicMock(return_value={"currentDateTime": "20.04.2020T13:53Z"})
         self.assertEqual(2020, what_is_year_now())
 
-    def test_type(self):
-        json.load = MagicMock(return_value={"currentDateTime": 'T13:53Z'})
-        self.assertRaises(ValueError, return_value={"currentDateTime": 'T13:53Z'})
+    def test_get_data(self):
+        json.load = MagicMock(return_value={"currentDateTime": "2025 04-20T13:53Z"})
+        self.assertRaises(ValueError, what_is_year_now)
 
 
 if __name__ == '__main__':
